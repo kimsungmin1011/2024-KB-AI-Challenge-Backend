0075ba4ca3f2094a26d1244694aa3c8a860693f2
전민수 <79839332+alstntjddms@users.noreply.github.com>
2024-08-07 20:59:59 +0900

    환경설정 수정
    java17 -> java21
    port => 30901
    context-path =>/api/kb-ai/challenge

--- src/main/java/com/example/demo/dart/controller/CorporationController.java
+++ src/main/java/com/example/demo/dart/controller/CorporationController.java
@@ -13,7 +13,7 @@ import java.util.List;
 import java.util.Map;
 
 @RestController
-@RequestMapping("/api/corporation")
+@RequestMapping("/corporation")
 public class CorporationController {
 
     private final CorporationService corporationService;
